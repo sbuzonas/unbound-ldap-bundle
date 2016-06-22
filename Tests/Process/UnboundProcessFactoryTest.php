1438fcd016fdfff45b7467c2ebb9bd145d41fa91
Steve Buzonas <sbuzonas@carnegielearning.com>
2016-06-22 15:00:27 -0400

    test unbound process factory

--- Tests/Process/UnboundProcessFactoryTest.php
+++ Tests/Process/UnboundProcessFactoryTest.php
@@ -3,43 +3,74 @@
 namespace CarnegieLearning\UnboundLdapBundle\Tests\Process;
 
 use CarnegieLearning\UnboundLdapBundle\Process\JavaExecutableFinder;
+use CarnegieLearning\UnboundLdapBundle\Process\ProcessBuilderFactory;
 use CarnegieLearning\UnboundLdapBundle\Process\UnboundProcessFactory;
+use Symfony\Component\Process\ProcessBuilder;
 
 /**
  * @backupGlobals enabled
  * @coversDefaultClass CarnegieLearning\UnboundLdapBundle\Process\UnboundProcessFactory
+ * @uses CarnegieLearning\UnboundLdapBundle\Process\UnboundProcessFactory
  */
 class UnboundProcessFactoryTest extends \PHPUnit_Framework_TestCase
 {
 
+    protected $builder;
+
+    protected $finder;
+
+    protected $fixture;
+
+    protected $processFactory;
+
+    public function setUp()
+    {
+        $this->finder = $this->createMock(JavaExecutableFinder::class);
+        $this->builder = $this->createMock(ProcessBuilder::class);
+
+        $processFactory = $this->createMock(ProcessBuilderFactory::class);
+        $processFactory->expects($this->once())->method('create')->willReturn($this->builder);
+
+        $this->fixture = new UnboundProcessFactory($this->finder, $processFactory);
+    }
+
     /**
      * @covers ::__construct
-     * @uses CarnegieLearning\UnboundLdapBundle\Process\UnboundProcessFactory::create
-     * @uses CarnegieLearning\UnboundLdapBundle\Process\UnboundProcessFactory::getJavaRuntime
      */
     public function testConstruct()
     {
-        $finder = $this->createMock(JavaExecutableFinder::class);
-        $finder->expects($this->once())->method('find')->willReturn('/path/to/java');
-
-        $fixture = new UnboundProcessFactory($finder);
-        $fixture->create();
+        $this->finder->expects($this->once())->method('find')->willReturn('/path/to/java');
+        $this->fixture->create();
     }
 
     /**
      * @covers ::getJavaRuntime
-     * @uses CarnegieLearning\UnboundLdapBundle\Process\UnboundProcessFactory::__construct
-     * @uses CarnegieLearning\UnboundLdapBundle\Process\UnboundProcessFactory::create
      *
      * @expectedException Symfony\Component\Process\Exception\RuntimeException
      * @expectedExceptionMessage Unable to find Java binary to run server.
      */
     public function testGetJavaRuntime_ThrowsExceptionWhenNotFound()
     {
-        $finder = $this->createMock(JavaExecutableFinder::class);
-        $finder->expects($this->once())->method('find')->willReturn(false);
+        $this->finder->expects($this->once())
+            ->method('find')
+            ->willReturn(false);
+        $this->fixture->create();
+    }
 
-        $fixture = new UnboundProcessFactory($finder);
-        $fixture->create();
+    /**
+     * @covers ::create
+     * @covers ::getJavaRuntime
+     */
+    public function testCreate()
+    {
+        $this->finder->expects($this->once())
+            ->method('find')
+            ->willReturn('/path/to/java');
+
+        $this->builder->expects($this->once())
+            ->method('setPrefix')
+            ->with($this->equalTo('/path/to/java'));
+
+        $this->assertSame($this->builder, $this->fixture->create());
     }
 }
